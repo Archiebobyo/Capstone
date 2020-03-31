f1aae63c62297031b42be2c9e6fec0076e441e33
GregoryGruse <gog12@my.fsu.edu>
2020-03-31 14:23:14 -0400

    <ade some adjustments to mysql connection tester. Also started work on main app. It currently has a login screen, main menu, and a bunch of blank screens

---
+++ Baker Bank/Baker Bank/LoginForm.vb
@@ -0,0 +1,62 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class LoginForm
+    Dim MysqlConn As MySqlConnection
+    Dim COMMAND As MySqlCommand
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MysqlConn = New MySqlConnection
+        MysqlConn.ConnectionString = "server=bakerybank1.c0sydhyi1pnd.us-east-2.rds.amazonaws.com;userid=admin;password=TINtin343!;database=mydb"
+
+        Try
+            MysqlConn.Open()
+            MessageBox.Show("Connection Successful")
+            MysqlConn.Close()
+        Catch ex As MySqlException
+            MessageBox.Show(ex.Message)
+        Finally
+            MysqlConn.Dispose()
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MysqlConn = New MySqlConnection
+        MysqlConn.ConnectionString = "server=bakerybank1.c0sydhyi1pnd.us-east-2.rds.amazonaws.com;userid=admin;password=TINtin343!;database=mydb"
+        Dim READER As MySqlDataReader
+        Try
+            MysqlConn.Open()
+            Dim Query As String
+            Query = "select * from mydb.Employee where email='" & TextBox1.Text & "' and password='" & TextBox2.Text & "' "
+            COMMAND = New MySqlCommand(Query, MysqlConn)
+            READER = COMMAND.ExecuteReader
+            Dim count As Integer
+            count = 0
+            While READER.Read
+                count = count + 1
+            End While
+
+            If count = 1 Then
+                MessageBox.Show("UserName And Password are correct")
+                Dim MainMenu As New MainMenuForm
+                MainMenu.Show()
+                Me.Close()
+            ElseIf count > 1 Then
+                MessageBox.Show("UserName And Password are Duplicate")
+            Else
+                MessageBox.Show("UserName And Password are not correct")
+            End If
+
+
+            MysqlConn.Close()
+
+        Catch ex As MySqlException
+            MessageBox.Show(ex.Message)
+        Finally
+            MysqlConn.Dispose()
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox2.UseSystemPasswordChar = True
+    End Sub
+End Class
---
+++ Baker Bank/Baker Bank/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Baker Bank")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Baker Bank")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("531435e0-f386-4c52-bc8d-0fc7e33a9251")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
