8919a3477f54a3ce1103659b00d1e047eef76f21
GregoryGruse <gog12@my.fsu.edu>
2020-03-31 14:23:14 -0400

    Made some adjustments to mysql connection tester. Also started work on main app. It currently has a login screen, main menu, and a bunch of blank screens

---
+++ Baker Bank/Baker Bank/MainMenuForm.vb
@@ -0,0 +1,25 @@
+﻿Public Class MainMenuForm
+    Private Sub currentInventoryBtn_Click(sender As Object, e As EventArgs) Handles currentInventoryBtn.Click
+        Dim Form As New currentInventoryForm
+        Form.Show()
+        Me.Close()
+    End Sub
+
+    Private Sub orderFormBtn_Click(sender As Object, e As EventArgs) Handles orderFormBtn.Click
+        Dim Form As New OrderForm
+        Form.Show()
+        Me.Close()
+    End Sub
+
+    Private Sub customerOrderBtn_Click(sender As Object, e As EventArgs) Handles customerOrderBtn.Click
+        Dim Form As New CustomerOrderForm
+        Form.Show()
+        Me.Close()
+    End Sub
+
+    Private Sub logoutBtn_Click(sender As Object, e As EventArgs) Handles logoutBtn.Click
+        Dim Form As New LoginForm
+        Form.Show()
+        Me.Close()
+    End Sub
+End Class
